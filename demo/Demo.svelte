f1778d6d74e523fe758146c1ba0bc56bf64e17a9
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2022-05-20 18:38:02 +0200

    fix: configure API key in config

--- demo/Demo.svelte
+++ demo/Demo.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
     import RecogWidget from '../lib/RecogWidget.svelte';
+    import config from './config.json';
+
+    let error: Error;
+    if (!config.api.key) {
+        error = new Error('Please setup your Vision API key first');
+    }
 
     //generate API URL statically
-    const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=`
+    const apiUrl = `${config.api.url}?key=${config.api.key}`;
 </script>
 
-<h1 class="text-center text-xl" >Demo application for the Recog Widget</h1>
-<div class="w-screen max-w-[50%] my-6 mx-auto" >
-    <RecogWidget {apiUrl} />
+<h1 class="text-center text-xl">Demo application for the Recog Widget</h1>
+<div class="w-screen max-w-[50%] my-6 mx-auto">
+    {#if error}
+        <p class="text-red">{error.message}</p>
+    {:else}
+        <RecogWidget {apiUrl} />
+    {/if}
 </div>
